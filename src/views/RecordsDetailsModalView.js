3e14afbf2ae8f7fcd85edd224b60d7f4ddb9a05d
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2017-01-13 11:36:50 +0100

    Tweaking SearchResult modal to allow to add record to selection.

--- src/views/RecordsDetailsModalView.js
+++ src/views/RecordsDetailsModalView.js
@@ -23,6 +23,7 @@ const RecordsDetailsModalView = ModalView.extend({
     'click .records-next': 'onRecordsNextClicked',
     'click .records-prev': 'onRecordsPrevClicked',
     'shown.bs.modal': 'onModalShown',
+    'change .is-selected': 'onDownloadSelectionChange',
   },
 
   initialize(options) {
@@ -33,54 +34,76 @@ const RecordsDetailsModalView = ModalView.extend({
     this.overlayLayersCollection = options.overlayLayersCollection;
     this.layersCollection = options.layersCollection;
     this.highlightStrokeColor = options.highlightStrokeColor;
+
+    this.mapModel = new MapModel({ center: [0, 0], zoom: 5 });
+    this.highlightModel = new HighlightModel();
+    this.filtersModel = new FiltersModel();
   },
 
   onModalShown() {
-    this.updateRecord(this.records[this.currentRecordIndex]);
+    this.updateRecord(...this.records[this.currentRecordIndex]);
   },
 
-  updateRecord(record) {
-    const time = record.get('properties').time;
-    const mapModel = new MapModel({ center: [0, 0], zoom: 5 });
-    const highlightModel = new HighlightModel();
-    const detailsView = new RecordDetailsView({
-      model: record,
-      mapModel,
-      mapView: new OpenLayersMapView({
-        mapModel,
-        filtersModel: new FiltersModel({ time }),
-        highlightModel,
+  updateRecord(recordModel, searchModel) {
+    // if (!this.mapView) {
+      this.mapView = new OpenLayersMapView({
+        mapModel: this.mapModel,
+        filtersModel: this.filtersModel,
+        highlightModel: this.highlightModel,
         baseLayersCollection: this.baseLayersCollection,
         overlayLayersCollection: this.overlayLayersCollection,
         layersCollection: this.layersCollection,
         highlightFillColor: 'rgba(0, 0, 0, 0)',
         highlightStrokeColor: this.highlightStrokeColor,
-      }),
+      });
+    // }
+    const detailsView = new RecordDetailsView({
+      model: recordModel,
+      mapModel: this.mapModel,
+      mapView: this.mapView,
     });
     this.showChildView('content', detailsView);
 
-    mapModel.show(record.attributes);
-    highlightModel.highlight(record.attributes);
+    const time = recordModel.get('properties').time;
+    const layerModel = searchModel.get('layerModel');
 
-    // this.$('.modal-title').text(`${layerModel.get('displayName')} - ${time[0].toISOString()}`);
-    this.$('.modal-title').text(`- ${time[0].toISOString()}`);
+    this.mapModel.show(recordModel.attributes);
+    this.highlightModel.highlight(recordModel.attributes);
+    this.filtersModel.set('time', time);
+
+    this.$('.modal-title').text(`${layerModel.get('displayName')} - ${time[0].toISOString()}`);
     this.$('.records-prev').toggleClass('disabled', !(this.currentRecordIndex > 0));
     this.$('.records-next').toggleClass('disabled', !(this.currentRecordIndex < this.records.length - 1));
+
+
+    const downloadSelection = searchModel.get('downloadSelection');
+    const isSelectedForDownload = downloadSelection.findIndex(model => (
+      model.get('id') === recordModel.get('id')
+    )) !== -1;
+    this.$('.is-selected').prop('checked', isSelectedForDownload);
   },
 
   onRecordsPrevClicked() {
     if (this.currentRecordIndex > 0) {
       this.currentRecordIndex -= 1;
-      const record = this.records[this.currentRecordIndex];
-      this.updateRecord(record);
+      this.updateRecord(...this.records[this.currentRecordIndex]);
     }
   },
 
   onRecordsNextClicked() {
     if (this.currentRecordIndex < this.records.length - 1) {
       this.currentRecordIndex += 1;
-      const record = this.records[this.currentRecordIndex];
-      this.updateRecord(record);
+      this.updateRecord(...this.records[this.currentRecordIndex]);
+    }
+  },
+
+  onDownloadSelectionChange() {
+    const [recordModel, searchModel] = this.records[this.currentRecordIndex];
+    const downloadSelection = searchModel.get('downloadSelection');
+    if (this.$('.is-selected').is(':checked')) {
+      downloadSelection.add(recordModel);
+    } else {
+      downloadSelection.remove(recordModel);
     }
   },
 });
