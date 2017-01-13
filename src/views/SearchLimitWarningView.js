d3150dc63025a5e13c663e9720df637e3e306a4c
Stephan Meißl <stephan@meissl.name>
2017-01-13 15:14:00 +0100

    Move topPanel style to css file and adjust it.

--- src/views/SearchLimitWarningView.js
+++ src/views/SearchLimitWarningView.js
@@ -1,5 +1,7 @@
 import Marionette from 'backbone.marionette';
 
+require('./SearchLimitWarningView.css');
+
 export default Marionette.ItemView.extend({
-  template: () => '<div class="alert alert-danger">Search hit more records than are actually shown</div>',
+  template: () => '<div class="alert alert-danger">Narrow down search to show all results</div>',
 });
