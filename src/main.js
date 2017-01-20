3a3a15062afc3ef5c09f83b3b64b770def840aec
Stephan Meißl <stephan@meissl.name>
2017-01-20 16:24:16 +0100

    Adjusting warnings and adding German version.

--- src/main.js
+++ src/main.js
@@ -341,14 +341,14 @@ window.Application = Marionette.Application.extend({
     filtersModel.on('change', () => {
       // show warning when time filter is set
       warningsCollection.setWarning(
-        i18next.t('Using time filter. Displayed records may differ from map.'),
+        i18next.t('timefilter_warning'),
         filtersModel.get('time') || false
       );
 
       const otherFilters = Object.keys(filtersModel.attributes)
         .filter(key => key !== 'time' && key !== 'area');
       warningsCollection.setWarning(
-        i18next.t('Special filters are set, background map may not reflect this.'),
+        i18next.t('advancedfilter_warning'),
         otherFilters.length
       );
     });
@@ -361,7 +361,7 @@ window.Application = Marionette.Application.extend({
         .reduce((acc, searchModel) => (
           acc || searchModel.get('totalResults') > searchModel.get('hasLoaded')
         ), false);
-      warningsCollection.setWarning(i18next.t('Search hit too many records'), show);
+      warningsCollection.setWarning(i18next.t('toomanyresults_warning'), show);
     });
 
 
