de26e7f9ff78065a630b39db2ab66ef5dad10f32
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2017-02-02 11:48:28 +0100

    Added translations. Improved 't' template helper to allow pluralization.

--- src/helpers/t.js
+++ src/helpers/t.js
@@ -1,5 +1,8 @@
 import i18next from 'i18next';
 
-export default function (key) {
+export default function (key, count = undefined) {
+  if (typeof count === 'number') {
+    return i18next.t(key, { count });
+  }
   return i18next.t(key);
 }
