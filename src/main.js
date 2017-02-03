aa949b35e40047b9f14f9ea1b0896421983d3676
Stephan Meißl <stephan@meissl.name>
2017-02-03 11:23:48 +0100

    Adjusting ordering of selection and hover highlight style.

--- src/main.js
+++ src/main.js
@@ -382,13 +382,13 @@ window.Application = Marionette.Application.extend({
     // create a dynamic style to set up the border/background color of record
     // items in the search results and download selection view.
     $(`<style>
-      .record-item:hover, .record-item.highlighted {
-        background-color: ${settings.highlightFillColor};
-        border-color: ${settings.highlightStrokeColor};
-      }
       .record-item.selected-for-download {
         background-color: ${settings.selectedFootprintFillColor};
         border-color: ${settings.selectedFootprintStrokeColor};
+      }
+      .record-item:hover, .record-item.highlighted {
+        background-color: ${settings.highlightFillColor};
+        border-color: ${settings.highlightStrokeColor};
       }
       </style>
     `).appendTo('head');
