a8275f4a49ac61141d7237ab0f79a63b4a955723
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2017-02-16 09:54:52 +0100

    Improving vendor info: Using background SVG for icon to fix issues with IE11.

--- src/views/VendorInfoView.js
+++ src/views/VendorInfoView.js
@@ -2,18 +2,14 @@ import Marionette from 'backbone.marionette';
 
 import template from './VendorInfoView.hbs';
 import './VendorInfoView.css';
-import eyePath from '../eox/EOX_Logo_eye_only.svg';
 
 const VendorInfoView = Marionette.ItemView.extend({
   template,
-  templateHelpers: {
-    eyePath,
-  },
   events: {
     'click .toggle-collapse': 'onToggleCollapseClicked',
   },
   onToggleCollapseClicked() {
-    this.$('.collapse,.arrow-collapse,.icon-eox-eye').toggle();
+    this.$('.collapse,.arrow-collapse,.icon-eox-eye-white').toggle();
   }
 });
 
