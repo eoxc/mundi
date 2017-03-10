0c88bd82a2ede423029b3216c80a24eff294a3c3
Stephan Meißl <stephan@meissl.name>
2017-03-10 15:53:43 +0100

    Adding tutorial step for layers.

--- src/tutorial.js
+++ src/tutorial.js
@@ -50,16 +50,36 @@ export default function getTutorialWidget() {
         if (!$('#rightPanel button').hasClass('out')) {
           $('#rightPanel button')[0].click();
         }
+        if (!$('#rightPanel .nav.nav-tabs :nth-child(1)').hasClass('active')) {
+          $('#rightPanel .nav.nav-tabs :nth-child(1) a').click();
+        }
       },
     },
     {
       target: '#leftPanel .side-panel-left',
       position: 'right',
       content: i18next.t('tutorial5'),
+      buttons: [AnnoButton.BackButton, AnnoButton.DoneButton, AnnoButton.NextButton],
+      onShow: () => {
+        if (!$('#leftPanel button').hasClass('out')) {
+          $('#leftPanel button')[0].click();
+        }
+        if (!$('#leftPanel .nav.nav-tabs :nth-child(1)').hasClass('active')) {
+          $('#leftPanel .nav.nav-tabs :nth-child(1) a').click();
+        }
+      },
+    },
+    {
+      target: '#leftPanel .side-panel-left',
+      position: 'right',
+      content: i18next.t('tutorial6'),
       buttons: [AnnoButton.BackButton, finishButton],
       onShow: () => {
         if (!$('#leftPanel button').hasClass('out')) {
           $('#leftPanel button')[0].click();
+        }
+        if (!$('#leftPanel .nav.nav-tabs :nth-child(2)').hasClass('active')) {
+          $('#leftPanel .nav.nav-tabs :nth-child(2) a').click();
         }
       },
     },
