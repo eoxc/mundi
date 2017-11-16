950d44c1563dc9db36f389bee73708a5a9644207
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2017-11-16 17:08:25 +0100

    Small fix.

--- src/views/filters/BaseExtraParameterView.js
+++ src/views/filters/BaseExtraParameterView.js
@@ -2,7 +2,13 @@ import Marionette from 'backbone.marionette';
 
 const BaseExtraParameterView = Marionette.ItemView.extend({
   templateHelpers() {
-    const result = this.model.get('name').replace(/([A-Z])/g, ' $1');
+    let name = this.model.get('name');
+    if (!name) {
+      const type = this.model.get('type');
+      name = type; // TODO
+    }
+
+    const result = name.replace(/([A-Z])/g, ' $1');
     return {
       displayName: result.charAt(0).toUpperCase() + result.slice(1),
     };
