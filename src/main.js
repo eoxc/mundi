b759d4d2bfb75c41028f3385f94ca450f24e39bf
Lubomir Bucek <lubomir.bucek@eox.at>
2019-09-20 12:35:20 +0200

    package update

--- src/main.js
+++ src/main.js
@@ -576,7 +576,7 @@ window.Application = Marionette.Application.extend({
     // use set timeout here so that vendor info is always at the end of the attribution list
     setTimeout(() => {
       const vendorInfoHTML = `<li>Powered&nbsp;by&nbsp;<a href="https://github.com/eoxc" target="_blank">EOxC</a>&nbsp;&copy;&nbsp;<a href="https://eox.at" target="_blank">EOX&nbsp;<i class="icon-eox-eye"/></a>
-      <!-- mundi Client version ${cdeVersion} https://github.com/eoxc/mundi/releases/tag/v${cdeVersion} -->
+      <!-- prism Client version ${cdeVersion} https://github.com/eoxc/prism/releases/tag/v${cdeVersion} -->
       <!-- eoxc version ${eoxcVersion} https://github.com/eoxc/eoxc/releases/tag/v${eoxcVersion} --></li>`;
       $(this.container).find('.ol-attribution ul').append(vendorInfoHTML);
     });
