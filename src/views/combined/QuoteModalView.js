81f37a386bb3e8b94d4515d532d2050cfae4be24
Lubomir Bucek <lubomir.bucek@eox.at>
2021-03-31 12:00:00 +0200

    add ui & saving of area in case of no product selected for quote

--- src/views/combined/QuoteModalView.js
+++ src/views/combined/QuoteModalView.js
@@ -12,6 +12,7 @@ const QuoteModalView = ModalView.extend({
       productIds: this.productIds,
       url: this.layerModel.get('download.url'),
       quoteModalIntro: this.layerModel.get('download.quoteModalIntro'),
+      area: this.area,
     };
   },
 
@@ -20,17 +21,34 @@ const QuoteModalView = ModalView.extend({
 
   events: {
     'click .copy-clipboard': 'onCopyClipboardClicked',
+    'click .copy-clipboard-area': 'onCopyClipboardAreaClicked',
   },
 
   initialize(options) {
     this.records = options.records;
     this.productIds = this.records.models.map(item => item.get('id'));
     this.layerModel = this.records.searchModel.get('layerModel');
+    let actualWindowObject = window;
+    if (window.self !== window.top) { // checking if it is an iframe
+      actualWindowObject = window.top;
+    }
+    // extracts search area from url
+    const params = new URLSearchParams(actualWindowObject.location.search);
+    this.area = params.get('area');
   },
 
   onCopyClipboardClicked() {
     const toCopy = this.productIds.join(', ');
     const copied = copyToClipboard(toCopy);
+    if (copied) {
+      this.$('.copy-successful').show().delay(5000).fadeOut();
+    } else {
+      this.$('.copy-unsuccessful').show().delay(8000).fadeOut();
+    }
+  },
+
+  onCopyClipboardAreaClicked() {
+    const copied = copyToClipboard(this.area);
     if (copied) {
       this.$('.copy-successful').show().delay(5000).fadeOut();
     } else {
